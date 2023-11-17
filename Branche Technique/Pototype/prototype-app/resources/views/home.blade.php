b71af4e71ead2179fe7bd095ac4c14b45350b1c5
Hamid Achaou <hamidAchaou379@gmail.com>
2023-11-17 10:53:41 +0100

    installation prototype app cnmh

---
+++ Branche Technique/Pototype/prototype-app/resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid pt-4">
+        <h1 class="text-black-50">Bienvenue dans votre candidature!</h1>
+    </div>
+@endsection
+ 
